3b54d1be28eb491972ff48db9208aea0e165c9b9
Arsen <arsenvakhidov2005@gmail.com>
2024-05-03 11:48:09 +0300

    testing for deploy

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, RouterProvider } from "react-router-dom";
+import { Bonjour } from './Bonjour';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   //<React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <App />
+    // </BrowserRouter>
+    <Bonjour/>
   //</React.StrictMode>
 );
