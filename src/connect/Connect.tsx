e0b633bc54d3c0da8a418a57e28b5fbf6b785ffd
Arsen_home <arsenvakhidov2005@gmail.com>
2024-05-15 14:17:58 +0300

    deploy with deployed server

--- src/connect/Connect.tsx
+++ src/connect/Connect.tsx
@@ -2,56 +2,106 @@ import React from "react";
 import axios from "axios";
 import { ExpertAndInterestedForServerT, Theme, UserAndTheme, UserAndThemeForServerT, UserAuthorizationT, UserRegistrationT, UserT } from "../types/user";
 const url = 'http://127.0.0.1:5000/api/registration'
+const server = 'https://expertssiteback-04981a476ea5.herokuapp.com'
 //const testUrl = 'https://httpbin.org/post'
 
 export const Connect = {
     async axiosAddUser(data: UserRegistrationT) {
-        return axios.post(url, {
+        return axios.post(`${server}/api/registration`, {
             'data': data
         });
     },
     async axiosGetUser() {
-        return axios.get('http://127.0.0.1:5000/get_users');
+        return axios.get(`${server}/get_users`);
     },
     async axiosDelete(user_id: number) {
-        return axios.delete(`http://127.0.0.1:5000/api/delete/${user_id}`,
+        return axios.delete(`${server}/api/delete/${user_id}`,
         );
     },
     async axiosClear() {
-        return axios.delete('http://127.0.0.1:5000/clear_table');
+        return axios.delete(`${server}/clear_table`);
     },
     async axiosAddTheme(data: Theme) {
-        return axios.post('http://127.0.0.1:5000/add-theme', {
+        return axios.post(`${server}/add-theme`, {
             'data': data 
         });
     },
     async axiosGetTheme() {
-        return axios.get('http://127.0.0.1:5000/get_themes');
+        return axios.get(`${server}/get_themes`);
     },
     async axiosDeleteExpertAndInterested(user_id: number) {
-        return axios.delete(`http://127.0.0.1:5000/delete_expert_and_interested/${user_id}`);
+        return axios.delete(`${server}/delete_expert_and_interested/${user_id}`);
     },
     async axiosStayExpertOrInterested(data: ExpertAndInterestedForServerT) {
-        return axios.post('http://127.0.0.1:5000/stay_expert_and_interested', {
+        return axios.post(`${server}/stay_expert_and_interested`, {
             'data': data
         });
     },
     async axiosAuthorization(data: UserAuthorizationT) {
-        return axios.post('http://127.0.0.1:5000/authorization', {
+        return axios.post(`${server}/authorization`, {
             'data': data
         })
     },
     async axiosPostToken(data: number) {
-        return axios.post('http://127.0.0.1:5000/read_token', {
+        return axios.post(`${server}/read_token`, {
             'id': data
         })        
     },
     async axiosGetUserById(user_id: number) {
-        return axios.get(`http://127.0.0.1:5000/one_user/${user_id}`)
+        return axios.get(`${server}/one_user/${user_id}`)
     },
     async axiosGetUserByExpert(theme_id: number[] | undefined) {
-        return axios.post('http://127.0.0.1:5000/get_users_by_interested', {
+        return axios.post(`${server}/get_users_by_interested`, {
             'idTheme': theme_id
         })
     }
+    // async axiosAddUser(data: UserRegistrationT) {
+    //     return axios.post(url, {
+    //         'data': data
+    //     });
+    // },
+    // async axiosGetUser() {
+    //     return axios.get('http://127.0.0.1:5000/get_users');
+    // },
+    // async axiosDelete(user_id: number) {
+    //     return axios.delete(`http://127.0.0.1:5000/api/delete/${user_id}`,
+    //     );
+    // },
+    // async axiosClear() {
+    //     return axios.delete('http://127.0.0.1:5000/clear_table');
+    // },
+    // async axiosAddTheme(data: Theme) {
+    //     return axios.post('http://127.0.0.1:5000/add-theme', {
+    //         'data': data 
+    //     });
+    // },
+    // async axiosGetTheme() {
+    //     return axios.get('http://127.0.0.1:5000/get_themes');
+    // },
+    // async axiosDeleteExpertAndInterested(user_id: number) {
+    //     return axios.delete(`http://127.0.0.1:5000/delete_expert_and_interested/${user_id}`);
+    // },
+    // async axiosStayExpertOrInterested(data: ExpertAndInterestedForServerT) {
+    //     return axios.post('http://127.0.0.1:5000/stay_expert_and_interested', {
+    //         'data': data
+    //     });
+    // },
+    // async axiosAuthorization(data: UserAuthorizationT) {
+    //     return axios.post('http://127.0.0.1:5000/authorization', {
+    //         'data': data
+    //     })
+    // },
+    // async axiosPostToken(data: number) {
+    //     return axios.post('http://127.0.0.1:5000/read_token', {
+    //         'id': data
+    //     })        
+    // },
+    // async axiosGetUserById(user_id: number) {
+    //     return axios.get(`http://127.0.0.1:5000/one_user/${user_id}`)
+    // },
+    // async axiosGetUserByExpert(theme_id: number[] | undefined) {
+    //     return axios.post('http://127.0.0.1:5000/get_users_by_interested', {
+    //         'idTheme': theme_id
+    //     })
+    // }
 };
--- src/custom-hook/useUsersShow.tsx
+++ src/custom-hook/useUsersShow.tsx
@@ -43,8 +43,8 @@ export const useUsersShow = () => {
     const get_users = async() => {
       //const response = await Connect.axiosGetUser()
       const response = await Connect.axiosGetUserByExpert(user?.interests?.map((interested) => interested.id).flat())
-      //console.log(user)
-      //console.log(response.data[0])
+      //console.log(user?.interests?.map((interested) => interested.id).flat())
+      console.log(response.data)
       //response.data.map((user: UserFromServerT) => {console.log(user)})
       //console.log(response.data[1].experts[0])
       setUsers(response.data)
